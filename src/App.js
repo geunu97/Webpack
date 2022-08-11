6fd57dab8b7f9c58265cd7507937587e9fb1d865
olo90632951 <olo90632951@gmail.com>
2022-08-11 11:04:47 +0900

    로딩속도 비교

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-const App = () => {
+export const App = () => {
   const element = document.createElement("div");
   element.innerHTML = "<h2>App 컴포넌트</h2>";
 
   return element;
 };
-
-document.body.appendChild(App());
